ec621cdbcb0227ed47f4b8c5e66dda37f3e0d6b3
cleverson Cordeiro <cleversonkleber@gmail.com>
2025-07-02 19:50:10 -0300

    criação e configuração da entidade campanha

---
+++ src/main/java/com/campanha_insentivo/model/Campanha.java
@@ -0,0 +1,96 @@
+package com.campanha_insentivo.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "CAMPANHA")
+public class Campanha implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "NOME", nullable = false, unique = true)
+    private String nome;
+    @Column(name = "DATA_INICIO", nullable = false)
+    private LocalDateTime dataInicio;
+    @Column(name = "DATA_FIM", nullable = false)
+    private LocalDateTime dataFim;
+    @Column(name = "DESCRICAO", nullable = false, length = 255)
+    private String descricao;
+
+    public Campanha() {
+    }
+    
+    public Campanha(Long id, String nome, LocalDateTime dataInicio, LocalDateTime dataFim, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.descricao = descricao;
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public LocalDateTime getDataInicio() {
+        return dataInicio;
+    }
+    public void setDataInicio(LocalDateTime dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+    public LocalDateTime getDataFim() {
+        return dataFim;
+    }
+    public void setDataFim(LocalDateTime dataFim) {
+        this.dataFim = dataFim;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
+
+
+
+    @Override
+    public boolean equals(Object obj) {
+       if (this == obj) return true;
+       if (obj == null || getClass() != obj.getClass()) return false;
+       Campanha other = (Campanha) obj;
+       return Objects.equals(nome, other.nome); 
+    }
+
+    
+
+
+}
