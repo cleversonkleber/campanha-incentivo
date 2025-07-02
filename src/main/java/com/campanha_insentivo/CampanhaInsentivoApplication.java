d1645ffce04ad7b3ce7f97b0aac245450c9c33a9
cleverson Cordeiro <cleversonkleber@gmail.com>
2025-07-02 13:53:50 -0300

    v1

---
+++ src/main/java/com/campanha_insentivo/CampanhaInsentivoApplication.java
@@ -0,0 +1,13 @@
+package com.campanha_insentivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampanhaInsentivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampanhaInsentivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campanha_insentivo/CampanhaInsentivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campanha_insentivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampanhaInsentivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
