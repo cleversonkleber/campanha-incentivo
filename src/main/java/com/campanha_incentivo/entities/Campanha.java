2e05dca4001d46565e09ab8e15dd6499b34ce3cb
cleverson Cordeiro <cleversonkleber@gmail.com>
2025-10-13 23:18:14 -0300

    Correção join de id_participante para  id_campanha

--- src/main/java/com/campanha_incentivo/entities/Campanha.java
+++ src/main/java/com/campanha_incentivo/entities/Campanha.java
@@ -41,7 +41,7 @@ public class Campanha implements Serializable{
     @OneToMany
     @JoinTable(name = "TB_EVENTO",
 	     joinColumns = @JoinColumn(name = "id_evento"),
-	     inverseJoinColumns = @JoinColumn(name = "id_participante"))
+	     inverseJoinColumns = @JoinColumn(name = "id_campanha"))
     @Column(name = "EVENTO",nullable = true)
     private List<Evento> eventos; 
 
